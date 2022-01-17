aa1161b13b3e074a0588228fb06442a26dc54c3a
Vasmayer <lealimarco90@gmail.com>
2022-01-17 16:23:22 +0100

    add for to create cells

--- js/script.js
+++ js/script.js
@@ -8,24 +8,34 @@ Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro. */
 /* console.log('JS OK'); */
 
 /* FUNCTIONS */
+
+const getRandomNumber = (min,max) => Math.floor(Math.random() * (max - min)) + min;
 const createGrid = (columns,rows,gridElement) => 
 {
     for(let i = 0 ; i < rows ; i++)
     {
         for(let j = 0 ; j < columns ; j++)
         {
-            
+            let cell = document.createElement('div');
+            cell.style.width = `calc( 100% / ${columns}`;
+            cell.style.height = `calc( 100% / ${rows}`;
+            cell.className = 'cells';
+            cell.innerText = `1`
+            gridElement.appendChild(cell);
+
         }
     }
 } 
 
 
-/* RECUPERO GLI ELEMENTI DAL DOM */
+/* RECUPERO GLI ELEMENTI DAL DOM E CREO LE VARIABILI CHE MI SERVONO */
 const selectElement = document.getElementById('select');
+const listRdnNumbers = [];
 
 selectElement.addEventListener('change',(e) => {
 
-    const gridElement = document.getElementById('grind');
+    const gridElement = document.getElementById('grid');
+    gridElement.innerHTML = '';
     const selectValue = e.target.value; 
 
     if(selectValue)
@@ -42,13 +52,13 @@ selectElement.addEventListener('change',(e) => {
 
             case '2':
                 /* 9 X 9 */
-                createGrid(10,10,gridElement);
+                createGrid(9,9,gridElement);
 
             break;
 
             case '3':
                 /* 7 X 7 */
-                createGrid(10,10,gridElement);
+                createGrid(7,7,gridElement);
 
             break;
         }
